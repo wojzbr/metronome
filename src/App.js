5dd290c4dfde1577d7cad8787bf0767d48727080
Wojciech Zbrożek <wzbrozek@polsl.pl>
2021-11-22 12:47:01 +0100

    changed favicon and clause

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Metronome from './metronome/Metronome';
-
+import Clause from './clause/Clause'
 
 function App() {
 
   return (
     <div className="App">
       <Metronome />
+      <Clause />
     </div>
   );
 
---
+++ src/clause/Clause.js
@@ -0,0 +1,11 @@
+import './clause.css'
+
+const Clause = () => {
+    return (
+        <div id="clause">
+            <a target="_blank" href="https://icons8.com/icon/KYeWtplRceYV/metronome" rel="noreferrer">Metronome</a> favicon by <a target="_blank" href="https://icons8.com" rel="noreferrer">Icons8</a>
+        </div>
+    )
+}
+
+export default Clause;
